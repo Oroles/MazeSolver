6c48871235af3ff8b75fe3070d9de079e5a88cc7
Vultur Horatiu <vultur_horatiu@yahoo.com>
2014-10-16 14:48:30 +0200

    Add orientation for movement

--- movement.h
+++ movement.h
@@ -2,10 +2,14 @@
 #define _MOV_H
 
 #define THRESHOLD_DISTANCE 2
+typedef enum { UP, RIGHT, DOWN, LEFT } orientation;
 
 void stop();
 void move_forward(U32 power);
 void rotate_right(U32 power);
 void rotate_left(U32 power);
+
+orientation get_orientation();
+
 
 #endif
