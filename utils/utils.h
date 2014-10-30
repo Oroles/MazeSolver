12ea2616f6ab1202c66a7a73be2ffa5c4c652fed
Keylekan <q.chatelais@gmx.fr>
2014-10-30 14:35:17 +0100

    Update distance between the wheels

--- utils/utils.h
+++ utils/utils.h
@@ -9,7 +9,7 @@
 #define RAD 		(PI/180.0) 	// Conversion factor that translate angles from degrees to radians
 
 // About the robot
-#define W_DIST 		120 		// The distance between the wheels (in mm)
+#define W_DIST 		125 		// The distance between the wheels (in mm)
 #define W_DIAM		56 			// Nominal wheel diameter (in mm)
 #define W_PERI		178.0 		// Nominal wheel diameter (in mm)
 #define PULSES 		360 		// Encoder resolution (in pulses per revolution)
