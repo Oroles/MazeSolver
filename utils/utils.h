e3ddfd3a73dd59bcac21ccd8c9b874495332915c
Keylekan <q.chatelais@gmx.fr>
2014-11-07 10:51:46 +0100

    Add average wheels diameter (perimeter)
    Measured perimeter of the right wheel : 176
    Measured perimeter of the left wheel : 177
    Average perimeter : 176.5

--- utils/utils.h
+++ utils/utils.h
@@ -19,7 +19,7 @@
 // About the robot
 #define W_DIST 		125 		// The distance between the wheels (in mm)
 #define W_DIAM		56 			// Nominal wheel diameter (in mm)
-#define W_PERI		178.0 		// Nominal wheel diameter (in mm)
+#define W_PERI		176.5 		// Nominal wheel diameter (in mm)
 #define PULSES 		360 		// Encoder resolution (in pulses per revolution)
 #define CONV 		(W_PERI/PULSES)		// Conversion factor that translates encoder pulses into linear wheel displacement
 // Distance sensors
