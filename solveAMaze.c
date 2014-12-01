3c36b397de0c463b512f7471bc241b89ee8901a3
Keylekan <q.chatelais@gmx.fr>
2014-12-01 11:49:19 +0100

    Change main controller init + computational time

--- solveAMaze.c
+++ solveAMaze.c
@@ -44,15 +44,22 @@ void user_1ms_isr_type2(void){
 }
 
 TASK(MainController) {
+	U8 taskID=MAINCONTROL;
+
 	WaitEvent(CellCenter);
 	ClearEvent(CellCenter);
 	init_localization();
-	set_startMapping(true);
-	if(is_inside_square(get_realX(),get_realY(),CENTER_RES)) main_step();
+	// The robot is now in the center of the cell, oriented in the EAST
+	// Wait to have accurate values from Mapping
+	stop_during_period(2000);
+	WaitEvent(EndOfMovement);
+	ClearEvent(EndOfMovement);
+	main_step();
 	while(1)
 	{
 		WaitEvent(CellCenter);
 		ClearEvent(CellCenter);
+		start_time(taskID);
 		if(main_step()) {
 			main_end();
 			struct node* path = find_shortest_path(0,0,get_x(),get_y());
@@ -60,32 +67,58 @@ TASK(MainController) {
 			free_list(path);
 			TerminateTask();
 		}
+		stop_time(taskID);
 	}
 	TerminateTask();
 }
 
 TASK(Display) {
+	U8 taskID=DISPLAY;
+	start_time(taskID);
+
 	update_display();
+
+	stop_time(taskID);
 	TerminateTask();
 }
 
 TASK(Localization) {
+	U8 taskID=LOCALIZATION;
+	start_time(taskID);
+
 	update_localization();
+
+	stop_time(taskID);
 	TerminateTask();
 }
 
 TASK(Mapping) {
+	U8 taskID=MAPPING;
+	start_time(taskID);
+
 	update_map();
+
+	stop_time(taskID);
 	TerminateTask();
 }
 
 TASK(PIDControl) {
+	U8 taskID=PIDCONTROL;
+	start_time(taskID);
+
 	calculate_PID_output(get_mov_target_w());
+
+	stop_time(taskID);
 	TerminateTask();
 }
 
 TASK(Movement) {
+	U8 taskID=MOVEMENT;
+	start_time(taskID);
+
 	do_movement();
+
+	stop_time(taskID);
 	TerminateTask();
 }
 
