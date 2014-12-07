eeee0bc47e01d24feb3b142e679cc9954ebb43e9
Vultur Horatiu <vultur_horatiu@yahoo.com>
2014-12-07 12:15:13 +0100

    Revert "Remove locks"
    This reverts commit e90db4c3b480d5f0d507d44497dd9d41040349db.

--- slam/localization.c
+++ slam/localization.c
@@ -12,9 +12,13 @@
 double __rx=MAP_RES/2;
 int __x=0;
 	int get_x() {
+		GetResource(UpdateLocker);
+		ReleaseResource(UpdateLocker);
 		return __x;
 	}
 	double get_realX() {
+		GetResource(UpdateLocker);
+		ReleaseResource(UpdateLocker);
 		return __rx;
 	}
 	void update_x(double dx) {
@@ -26,9 +30,13 @@ int __x=0;
 double __ry=MAP_RES/2;
 int __y=0;
 	int get_y() {
+		GetResource(UpdateLocker);
+		ReleaseResource(UpdateLocker);
 		return __y;
 	}
 	double get_realY() {
+		GetResource(UpdateLocker);
+		ReleaseResource(UpdateLocker);
 		return __ry;
 	}
 	void update_y(double dy) {
@@ -190,8 +198,10 @@ void update_localization() {
 	}
 
 	// Don't stop me, I need synchronized parameter values
+	GetResource(RES_SCHEDULER);
 	int temp_wL=get_wPositionL();
 	int temp_wR=get_wPositionR();
+	ReleaseResource(RES_SCHEDULER);
 	
 	// Initialization
 	double wL,wR;
@@ -219,8 +229,10 @@ void update_localization() {
 		fix_localization(&x,&y,&__w);
 
 		// Update Shared Variables
+		GetResource(UpdateLocker);
 		update_x(x);
 		update_y(y);
 		update_w(__w/RAD);
+		ReleaseResource(UpdateLocker);
 	}
 }
